b733020964c01c00373eecd95b3a775256b5cb35
ruhamabek <ruhamauzubo@gmail.com>
2025-05-14 09:45:38 +0300

    Start project

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String name;
+    private int age;
+    private String studentId;
+
+    public Student(String name, int age, String studentId) {
+        this.name = name;
+        this.age = age;
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+}
